721645b7d9beb884d4f805706d84d14b60ae88f0
Zachary Huff <zach.huff.386@gmail.com>
2014-11-29 00:13:56 -0500

    Update build script

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -15,20 +15,15 @@ tar cfz pymongo_$VERSION.orig.tar.gz pymongo-$VERSION
 cp -r ../debian ./pymongo-$VERSION
 cd pymongo-$VERSION
 
+sed -i -e 's/0ubuntu1) unstable;/0ubuntu1~trusty) trusty;/g' debian/changelog
 debuild -S
-sed -i -e 's/0ubuntu1/0ubuntu1~precise/g' debian/changelog
-sed -i -e 's/trusty;/precise;/g' debian/changelog
-debuild -S
-sed -i -e 's/0ubuntu1~precise/0ubuntu1~utopic/g' debian/changelog
-sed -i -e 's/precise;/utopic;/g' debian/changelog
+sed -i -e 's/0ubuntu1~trusty) trusty;/0ubuntu1~precise) precise;/g' debian/changelog
 debuild -S
 
 cd ..
 
 echo '\n\nRUN COMMANDS BELOW TO UPLOAD:'
-echo 'sudo dput ppa:pritunl/ppa ./build/pymongo_'$VERSION'-0ubuntu1_source.changes'
+echo 'sudo dput ppa:pritunl/ppa ./build/pymongo_'$VERSION'-0ubuntu1~trusty_source.changes'
 echo 'sudo dput ppa:pritunl/ppa ./build/pymongo_'$VERSION'-0ubuntu1~precise_source.changes'
-echo 'sudo dput ppa:pritunl/ppa ./build/pymongo_'$VERSION'-0ubuntu1~utopic_source.changes'
-echo 'sudo dput ppa:pritunl/pritunl-testing ./build/pymongo_'$VERSION'-0ubuntu1_source.changes'
-echo 'sudo dput ppa:pritunl/pritunl-testing ./build/pymongo_'$VERSION'-0ubuntu1~precise_source.changes'
-echo 'sudo dput ppa:pritunl/pritunl-testing ./build/pymongo_'$VERSION'-0ubuntu1~utopic_source.changes'
+echo 'sudo dput ppa:pritunl/pritunl-dev ./build/pymongo_'$VERSION'-0ubuntu1~trusty_source.changes'
+echo 'sudo dput ppa:pritunl/pritunl-dev ./build/pymongo_'$VERSION'-0ubuntu1~precise_source.changes'
